8c7ef26c537dfa1634b988db31f3872b32d5715f
Paula Suarez <Paula.Suarez@mainevent.com>
2020-10-15 17:53:56 -0500

    Adding updates

---
+++ scripts.ts
@@ -0,0 +1,12 @@
+function absoluteValuesSumMinimization(a: number[]): number {
+
+    const even = a.length % 2 === 0;
+    
+    return even ? a[a.length / 2-1]: a[Math.floor(a.length /2)];
+
+}
+
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
